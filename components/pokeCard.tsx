7a31e1b77fc211433cfbb2141f5ad2646450cf87
islambou <islambou@gmail.com>
2021-05-14 18:04:16 +0200

    🐛 fix(poke card):  ios shadow

--- components/pokeCard.tsx
+++ components/pokeCard.tsx
@@ -1,5 +1,5 @@
-import React, { FC, useState } from "react";
-import { View, ViewProps, StyleSheet } from "react-native";
+import React, { FC } from "react";
+import { View, ViewProps, StyleSheet, Platform } from "react-native";
 import { TouchableOpacity } from "react-native-gesture-handler";
 import Text from "../components/text";
 import { PokemonBasic } from "../models/pokemon";
@@ -12,7 +12,16 @@ const styles = StyleSheet.create({
     backgroundColor: "white",
     borderRadius: 10,
     overflow: "hidden",
+  },
+  shadow: {
     elevation: 8,
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.2,
+    shadowRadius: 5,
   },
 });
 
@@ -24,18 +33,17 @@ interface PokeCardProps extends ViewProps {
 const PokeCard: FC<PokeCardProps> = ({ pokemon, ...props }) => {
   const cardColor = getPokeColor(pokemon.color.name);
   return (
-    <TouchableOpacity
-      onPress={props.onPress}
-      style={[styles.container, props.style]}
-    >
-      <View style={{ backgroundColor: cardColor }}>
-        <PokeImage pokemonId={pokemon.id} size={props.size} />
-      </View>
+    <TouchableOpacity onPress={props.onPress} style={styles.shadow}>
+      <View style={[styles.container, props.style]}>
+        <View style={{ backgroundColor: cardColor, overflow: "hidden" }}>
+          <PokeImage pokemonId={pokemon.id} size={props.size} />
+        </View>
 
-      <Text bold>{pokemon.name}</Text>
-      <Text bold subTitle size={10}>
-        #{pokemon.id}
-      </Text>
+        <Text bold>{pokemon.name}</Text>
+        <Text bold subTitle size={10}>
+          #{pokemon.id}
+        </Text>
+      </View>
     </TouchableOpacity>
   );
 };
